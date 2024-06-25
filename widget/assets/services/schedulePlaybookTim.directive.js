eaaa0d9d9505a7b3ef5d11de4936824ad8f4b56f
ypatodkar <ypatodkar@fortinet.com>
2024-06-25 15:41:26 +0530

    Initial commit

---
+++ widget/assets/services/schedulePlaybookTim.directive.js
@@ -0,0 +1,252 @@
+/* Copyright start
+  MIT License
+  Copyright (c) 2024 Fortinet Inc
+  Copyright end */
+  'use strict';
+
+  (function () {
+    angular
+      .module('cybersponse')
+      .directive('schedulePlaybookTim', schedulePlaybookTim);
+  
+    schedulePlaybookTim.$inject = ['$http', 'API', '$filter', '$window', 'CRUD_HUB', 'TimeZoneServices', 'SchedulesService',
+      'localStorageService', 'currentPermissionsService'];
+  
+    function schedulePlaybookTim ($http, API, $filter, $window, CRUD_HUB, TimeZoneServices, SchedulesService,
+      localStorageService, currentPermissionsService) {
+      var directive = {
+        restrict: 'A',
+        scope: {
+          viewWidgetVars: '<',
+          connectorsToSchedule: '<'
+        },
+        controller: 'BaseCtrl',
+        templateUrl: 'widgets/installed/threatIntelManagementConfiguration-1.0.0/widget/assets/services/schedulePlaybookTim.html',
+        link: link
+      };
+      function link(scope) {
+        scope.save = save;
+        scope.datetimeQuick = CRUD_HUB.DATETIME_QUICK;
+        scope.picklists = {};
+        scope.updateCron = updateCron;
+        scope.allowEdit = currentPermissionsService.availablePermission('schedules', 'create') || currentPermissionsService.availablePermission('schedules', 'update');
+        scope.dateField = {
+          'name': 'dateRange',
+          'title': 'Date Range',
+          'type': 'datetime.quick',
+          'editable': true
+        };
+        scope.hidePlaybook = false;
+        scope.scheduleConfig = {
+          name: 'Investigate_Outbreak-Alerts',
+          id: null,
+          crontab: {
+            minute: '01',
+            hour: '0',
+            day_of_week: '*',
+            day_of_month: '*',
+            month_of_year: '*'
+          },
+          kwargs: {}
+        };
+  
+        scope.params = {
+          updating: false,
+          action: 'Add',
+          playbookList: null,
+          defaultCronexpression: {
+            minute: '01',
+            hour: '0',
+            day_of_week: '*',
+            day_of_month: '*',
+            month_of_year: '*'
+          },
+          config: {},
+          cronexpression: {
+            minute: '01',
+            hour: '0',
+            day_of_week: '*',
+            day_of_month: '*',
+            month_of_year: '*'
+          },
+          timezones: {}
+        };
+        scope.params.config.cron = true;
+        scope.processingScheduleData = false;
+  
+        scope.$watch('scheduleConfig.kwargs.wf', function (newVal, oldVal) {
+          if (newVal && (oldVal && typeof oldVal === 'object')) {
+            scope.scheduleForm.$setDirty();
+          }
+        });
+  
+        function updateCron() {
+          var cronstrue = $window.cronstrue;
+          scope.cronDescriber = '';
+          if (scope.scheduleConfig.crontab.minute !== '' && scope.scheduleConfig.crontab.hour !== '' && scope.scheduleConfig.crontab.day_of_month !== '' && scope.scheduleConfig.crontab.month_of_year !== '' && scope.scheduleConfig.crontab.day_of_week !== '') {
+            scope.cronDescriber = cronstrue.toString(scope.scheduleConfig.crontab.minute + ' ' + scope.scheduleConfig.crontab.hour + ' ' + scope.scheduleConfig.crontab.day_of_month + ' ' + scope.scheduleConfig.crontab.month_of_year + ' ' + scope.scheduleConfig.crontab.day_of_week);
+          }
+          scope.params.config.cronName = angular.copy(scope.cronDescriber);
+        }
+  
+        function loadScheduleData() {
+          $http({
+            method: 'GET',
+            url: API.WORKFLOW + 'api/scheduled/?format=json&name=Investigate_Outbreak-Alerts'
+          }).then(function (response) {
+            if (response.data['hydra:member'] && response.data['hydra:member'].length > 0) {
+              scope.scheduleConfig.id = response.data['hydra:member'][0].id;
+              scope.$emit('scheduleDetails', { 'status': true, 'scheduleId': scope.scheduleConfig.id, 'scheduleFrequency': scope.cronDescriber });
+              scope.processingScheduleData = true;
+              SchedulesService.loadScheduleDetails(scope.scheduleConfig.id).then(function (result) {
+                scope.scheduleConfig = result;
+                scope.scheduleConfig.kwargs = scope.scheduleConfig.kwargs || {};
+                if (scope.scheduleConfig.kwargs.wf_iri) {
+                  var playbookId = '';
+                  if (scope.scheduleConfig.kwargs.wf_iri.indexOf(API.WORKFLOWS) > 0) {
+                    playbookId = $filter('getEndPathName')(scope.scheduleConfig.kwargs.wf_iri);
+                  } else {
+                    playbookId = scope.scheduleConfig.kwargs.wf_iri;
+                  }
+                  scope.scheduleConfig.kwargs.wf = API.API_3_BASE + API.WORKFLOWS + playbookId;
+                }
+                scope.processingScheduleData = false;
+                scope.scheduleConfig.kwargs.start_time = scope.scheduleConfig.kwargs.start_time || scope.scheduleConfig.start_time;
+                scope.scheduleConfig.kwargs.expires = scope.scheduleConfig.kwargs.expires || scope.scheduleConfig.expires;
+                scope.scheduleConfig.kwargs.timezone = scope.scheduleConfig.kwargs.timezone || scope.scheduleConfig.crontab.timezone;
+                updateCron();;
+              });
+            }
+            else {
+              updateCron();
+            }
+          });
+        }
+  
+        init();
+        function init() {
+          TimeZoneServices.getTimeZoneList().then(function (timezones) {
+            scope.params.timezones = timezones;
+            // loadScheduleData();
+          });
+        };
+  
+        scope.searchZoneName = function (event) {
+          event.preventDefault();
+          event.stopPropagation();
+          if (scope.fieldDropDown) {
+            scope.fieldDropDown.isopen = true;
+          }
+        };
+  
+        scope.selectTimezone = function (timezone) {
+          timezone = timezone || {};
+          scope.scheduleConfig.kwargs.timezone = timezone.utc;
+          scope.scheduleConfig.kwargs.utcOffset = timezone.utcOffset;
+          scope.setDateAsPerTimezone('expires');
+          scope.setDateAsPerTimezone('start_time');
+          scope.scheduleForm.$setDirty();
+        };
+  
+        scope.$watch('scheduleConfig.kwargs.expires', function () {
+          scope.setDateAsPerTimezone('expires');
+        });
+        scope.$watch('scheduleConfig.kwargs.start_time', function () {
+          scope.setDateAsPerTimezone('start_time');
+        });
+  
+        scope.setDateAsPerTimezone = function (dateKey) {
+          var ALL_COLONS = /:/g;
+          var timezone = scope.scheduleConfig.kwargs.utcOffset;
+          var requestedTimezoneOffset = null;
+          if (timezone) {
+            timezone = timezone.replace(ALL_COLONS, '');
+            requestedTimezoneOffset = Date.parse('Jan 01, 1970 00:00:00 ' + timezone) / 60000;
+          }
+          scope.scheduleConfig[dateKey] = null;
+          if (scope.scheduleConfig.kwargs[dateKey]) {
+            if (!angular.isDate(scope.scheduleConfig.kwargs[dateKey])) {
+              scope.scheduleConfig.kwargs[dateKey] = new Date(scope.scheduleConfig.kwargs[dateKey]);
+            }
+            var currentTimeZoneOffset = scope.scheduleConfig.kwargs[dateKey].getTimezoneOffset();
+            scope.scheduleConfig[dateKey] = angular.copy(scope.scheduleConfig.kwargs[dateKey]);
+            if (requestedTimezoneOffset) {
+              scope.scheduleConfig[dateKey].setMinutes(scope.scheduleConfig[dateKey].getMinutes() + ((requestedTimezoneOffset - currentTimeZoneOffset)));
+            }
+          }
+        };
+  
+        scope.setCronValue = function (field, key) {
+          angular.forEach(scope.scheduleConfig.crontab, function (cronVal, cronKey) {
+            if (cronKey === 'minute' || cronKey === 'hour' || cronKey === 'day_of_week' || cronKey === 'day_of_month' || cronKey === 'month_of_year') {
+              scope.scheduleConfig.crontab[cronKey] = '*';
+            }
+          });
+          if (key === 'daily') {
+            scope.scheduleConfig.crontab.minute = '1';
+            scope.scheduleConfig.crontab.hour = '0';
+          } else if (key === 'hourly') {
+            scope.scheduleConfig.crontab.minute = '0';
+            scope.scheduleConfig.crontab.hour = '*/1';
+          } else if (key === 'minute') {
+            scope.scheduleConfig.crontab.minute = '*/5';
+          } else if (key === 'weekly') {
+            scope.scheduleConfig.crontab.minute = '1';
+            scope.scheduleConfig.crontab.hour = '0';
+            scope.scheduleConfig.crontab.day_of_week = '1';
+          } else if (key === 'monthly') {
+            scope.scheduleConfig.crontab.minute = '1';
+            scope.scheduleConfig.crontab.hour = '0';
+            scope.scheduleConfig.crontab.day_of_month = '1';
+          } else if (key === 'yearly') {
+            scope.scheduleConfig.crontab.minute = '1';
+            scope.scheduleConfig.crontab.hour = '0';
+            scope.scheduleConfig.crontab.day_of_month = '1';
+            scope.scheduleConfig.crontab.month_of_year = '1';
+          }
+          scope.scheduleForm.$setDirty();
+          scope.updateCron();
+        };
+  
+        function save(scheduleForm) {
+          if (scheduleForm.$invalid) {
+            scheduleForm.$setTouched();
+            scheduleForm.$focusOnFirstError();
+            return;
+          }
+          scope.params.updating = true;
+          scope.scheduleConfig.enabled = true;
+          scope.scheduleConfig.kwargs.wf_iri = "/api/3/workflows/70d2c10e-50d4-43ce-b606-0f0d0d305fad";
+          var priority_payload = {
+            "@id": "/api/3/picklists/2b563c61-ae2c-41c0-a85a-c9709585e3f2",
+            "@type": "Picklist",
+            "itemValue": "Medium",
+            "orderIndex": 1,
+            "color": null,
+            "icon": null,
+            "listName": "/api/3/picklist_names/e104ef72-11b4-4d0c-be0e-e1cf3b87b5f2",
+            "uuid": "2b563c61-ae2c-41c0-a85a-c9709585e3f2",
+            "id": 111,
+            "importedBy": []
+          }
+          scope.scheduleConfig.kwargs.priority = priority_payload;
+          scope.scheduleConfig.crontab.timezone = scope.scheduleConfig.kwargs.timezone;
+          scope.scheduleConfig.kwargs.createUser = localStorageService.get(API.API_3_BASE + API.CURRENT_ACTOR)['@id'];
+          var scheduleData = angular.copy(scope.scheduleConfig);
+          if (scheduleData.kwargs.wf) {
+            delete scheduleData.kwargs.wf;
+          }
+          SchedulesService.saveSchedule(scheduleData).then(function (data) {
+            scope.scheduleConfig.id = data.id;
+            scope.status = true;
+            scope.$emit('scheduleDetails', { 'status': scope.status, 'scheduleId': scope.scheduleConfig.id, 'scheduleFrequency': scope.cronDescriber });
+            scope.params.updating = false;
+            _isScheduleModified = true;
+            scope.scheduleForm.$setPristine();
+          }
+          );
+        }
+      }
+      return directive;
+    }
+  })();
---
+++ widget/view.controller.js
@@ -0,0 +1,193 @@
+/* Copyright start
+  Copyright (C) 2008 - 2023 Fortinet Inc.
+  All rights reserved.
+  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  Copyright end */
+'use strict';
+(function () {
+  angular
+    .module('cybersponse')
+    .controller('threatIntelManagementConfiguration100Ctrl', threatIntelManagementConfiguration100Ctrl);
+
+  threatIntelManagementConfiguration100Ctrl.$inject = ['$scope', 'threatIntelManagementConfigurationService', 'WizardHandler', '$interval', '$controller', 'connectorService', 'currentPermissionsService'];
+
+  function threatIntelManagementConfiguration100Ctrl($scope, threatIntelManagementConfigurationService, WizardHandler, $interval, $controller, connectorService, currentPermissionsService) {
+    $controller('BaseConnectorCtrl', {
+      $scope: $scope
+    });
+
+    $scope.moveEnvironmentNext = moveEnvironmentNext;
+    $scope.movePrevious = movePrevious;
+    $scope.installConnector = installConnector;
+    $scope.loadConnectorDetails = loadConnectorDetails;
+    $scope.toggleAdvancedSettings = toggleAdvancedSettings;
+    $scope.saveConnector = saveConnector;
+    $scope.feedConnectors = {};
+    $scope.searchQuery = '';
+    init();
+
+    function init() {
+      threatIntelManagementConfigurationService.getFeedConnectors().then(function (response) {
+        $scope.feedConnectors = response.data['hydra:member'];
+      });
+    }
+
+    function toggleAdvancedSettings(connector, minimizeOthers) {
+      if (minimizeOthers && $scope.installedConnectors && $scope.installedConnectors.length > 0) {
+        $scope.installedConnectors.forEach(pack => {
+          pack.toggleAccordion = pack.name === connector.name;
+        });
+      }
+      connector.toggleAccordion = connector.toggleAccordion === undefined ? true : !connector.toggleAccordion;
+    }
+
+
+
+
+    function installConnector() {
+      moveEnvironmentNext();
+      $scope.getSelectedConnectorsCount = function () {
+        return $scope.feedConnectors.filter(function (connector) {
+          return connector.selectConnector;
+        }).length;
+      };
+      const selectedConnectors = $scope.feedConnectors.filter(pack => pack.selectConnector === true);
+
+      function installSequentially(index) {
+        if (index >= selectedConnectors.length) {
+          $scope.allConnectorsInstalled = true;
+          return; // All connectors have been processed
+        }
+        const connector = selectedConnectors[index];
+        threatIntelManagementConfigurationService.installConnector(connector)
+          .then(resp => {
+            const importJobId = resp.data.importJob;
+            threatIntelManagementConfigurationService.getInstallationProgress(importJobId, connector)
+              .then(() => {
+                connector.installed = true;
+                installSequentially(index + 1); // Move to the next connector
+              })
+              .catch(error => {
+                console.log(error);
+                // Continue to the next connector even if there is an error
+                installSequentially(index + 1);
+              });
+          })
+          .catch(error => {
+            console.log(error);
+            // Continue to the next connector even if there is an error
+            installSequentially(index + 1);
+          });
+      }
+      // installSequentially(0);
+    }
+
+    function loadConnectorDetails(connector) {
+      _loadConnectorDetails(connector.name, connector.version);
+      // _loadConnectorDetails("botvrij-misp-osint-feed", "1.0.0");
+    }
+
+    function _loadConnectorDetails(connectorName, connectorVersion, connectorDetails) {
+      $scope.processingConnector = true;
+      connectorService.getConnector(connectorName, connectorVersion).then(function (connector) {
+        if (!connector) {
+          toaster.error({
+            body: 'The Connector "' + connectorName + '" is not installed. Istall the connector and re-run thiz wizard to complete the configuration'
+          });
+          return;
+        }
+        $scope.selectedConnector = connector;
+        $scope.loadConnector($scope.selectedConnector, false, false);
+        $scope.processingConnector = false;
+      });
+    }
+
+    function saveConnector(saveFrom) {
+      $scope.isConnectorConfigured = true;
+      $scope.configuredConnector = false;
+      var data = angular.copy($scope.connector);
+      if (CommonUtils.isUndefined(data)) {
+        $scope.statusChanged = false;
+        return;
+      }
+      if (!currentPermissionsService.availablePermission('connectors', 'update')) {
+        $scope.statusChanged = false;
+        return;
+      }
+      var newConfiguration, newConfig, deleteConfig;
+      newConfiguration = false;
+      if (saveFrom !== 'deleteConfigAndSave') {
+        if (!_.isEmpty($scope.connector.config_schema)) {
+          if (!$scope.validateConfigurationForm()) {
+            return;
+          }
+        }
+        if (!$scope.input.selectedConfiguration.id) {
+          newConfiguration = true;
+          $scope.input.selectedConfiguration.config_id = $window.UUID.generate();
+          if ($scope.input.selectedConfiguration.default) {
+            angular.forEach(data.configuration, function (configuration) {
+              if (configuration.config_id !== $scope.input.selectedConfiguration.config_id) {
+                configuration.default = false;
+              }
+            });
+          }
+          data.configuration.push($scope.input.selectedConfiguration);
+          newConfig = $scope.input.selectedConfiguration;
+        }
+        delete data.newConfig;
+      }
+      if (saveFrom === 'deleteConfigAndSave') {
+        $scope.isConnectorConfigured = false;
+        deleteConfig = true;
+        $scope.isConnectorHealthy = false;
+      }
+      var updateData = {
+        connector: data.id,
+        name: $scope.input.selectedConfiguration.name,
+        config_id: $scope.input.selectedConfiguration.config_id,
+        id: $scope.input.selectedConfiguration.id,
+        default: $scope.input.selectedConfiguration.default,
+        config: {},
+        teams: $scope.input.selectedConfiguration.teams
+      };
+      $scope.saveValues($scope.input.selectedConfiguration.fields, updateData.config);
+      $scope.processing = true;
+      connectorService.updateConnectorConfig(updateData, newConfiguration, deleteConfig).then(function (response) {
+        if (newConfig) {
+          $scope.connector.configuration.push(newConfig);
+          if (newConfig.default) {
+            $scope.removeDefaultFromOthers();
+          }
+        }
+        $scope.formHolder.connectorForm.$setPristine();
+        if (!deleteConfig) {
+          $scope.input.selectedConfiguration.id = response.id;
+          $scope.configuredConnector = true;
+          $scope.isConnectorHealthy = true;
+        }
+        $scope.checkHealth();
+        $scope.statusChanged = false;
+      }, function (error) {
+        toaster.error({
+          body: error.data.message ? error.data.message : error.data['hydra:description']
+        });
+      }).finally(function () {
+        $scope.processing = false;
+      });
+    }
+
+
+    function movePrevious() {
+      WizardHandler.wizard('solutionpackWizard').previous();
+    }
+
+    function moveEnvironmentNext(filterInstalledConnectors) {
+      WizardHandler.wizard('solutionpackWizard').next();
+      if (filterInstalledConnectors) {
+        $scope.installedConnectors = $scope.feedConnectors.filter(connector => connector.installed === true);
+      }
+    }
+
+  }
+})();
